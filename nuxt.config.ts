a0a34c5c563a9a512b13f986e06bfdede18e62ca
v3nt <dan@jynk.net>
2023-09-20 14:28:25 +0000

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,30 @@
+import svgLoader from 'vite-svg-loader'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxtjs/html-validator',
+    '@nuxtjs/tailwindcss',
+    '@vueuse/nuxt',
+    '@pinia/nuxt',
+  ],
+  vite: {
+    plugins: [
+      svgLoader({
+        defaultImport: 'component',
+        svgoConfig: {
+          plugins: [
+            {
+              name: 'preset-default',
+              params: {
+                overrides: {
+                  removeViewBox: false,
+                },
+              },
+            },
+          ],
+        },
+      }),
+    ],
+  },
+})
